6e95ccfcbe7a96ec7fd2674ae18179873d05e581
sheraj hussein <sheraj.hussein@gmail.com>
2024-01-13 08:03:36 +0000

    added update with delete

--- script.js
+++ script.js
@@ -17,13 +17,17 @@ document.addEventListener("DOMContentLoaded", function () {
             const li = document.createElement('li');
             li.className = 'list-group-item d-flex justify-content-between align-items-center';
             li.innerHTML = `
-                    ${todo.name} <span class="badge bg-primary">${todo.urgency}</span>
-                    <button class='btn btn-warning edit-btn'>Edit</button>               
+                    <span>${todo.name}</span>
+                    <span class="badge bg-primary">${todo.urgency}</span>
+                    <div>
+                        <button class='btn btn-warning edit-btn'>Edit</button>
+                        <button class="btn delete-btn btn-danger btn-sm delete-btn">Delete</button>
+                    </div>
                 `;
             todoList.appendChild(li);
 
             // add event listener to edit btn
-            li.querySelector(".edit-btn").addEventListener('click', function() {
+            li.querySelector(".edit-btn").addEventListener('click', function () {
                 // alert("Hi " + todo.name)
                 const newName = prompt("Enter the new task name:", todo.name)
                 const newUrgency = prompt("Enter the new urgency: ", todo.urgency)
@@ -32,13 +36,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 renderTodos(todos)
             })
 
+            // add event listener to delete btn
+            li.querySelector(".delete-btn").addEventListener('click', function () {
+                // alert("Delete: " + todo.name)
+                const confirmation = confirm("Are you sure you want to delete this task: " + todo.name + " ?")
+                if (confirmation) {
+                    deleteTask(todos, todo.id)
+                    renderTodos(todos)
+                }
+            })
 
         }
     }
 
     // CREATE
     const addBtn = document.querySelector("#addTodo")
-    addBtn.addEventListener("click", function() {
+    addBtn.addEventListener("click", function () {
         const newTaskInput = document.querySelector("#taskName")
         const taskName = newTaskInput.value
 
